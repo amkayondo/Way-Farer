9cfd1e86e943dc64049f6ed2cb67cb45fd5f9f78
amkayondo <amakyondo01@gmail.com>
2019-07-05 23:33:43 +0300

    [finishes #167076378] add page to show specific page

--- UI/js/index.js
+++ UI/js/index.js
@@ -1,6 +1,10 @@
 const notLogedin = document.getElementById('nav-no-login');
 const notLtrips = document.getElementById('ntp--cnt');
 
+const loggedIn = (_div) => {
+    _div.innerHTML = ctn;
+}
+
 // Not loggedin nav
 notLogedin.innerHTML = `
 <a href="index.html"><div class="ntlg"><img src="./images/logo2.png" alt="Way Farer logo" class="ntlg-img">
@@ -16,8 +20,17 @@ notLogedin.innerHTML = `
 </div>`
 
 // trips, not logged in
+const tpst = `    <td class="t--hd">Bus</td>
+<td class="t--hd">Origin</td>
+<td class="t--hd">Destination</td>
+<td class="t--hd">Date</td>
+<td class="t--hd">Fare</td>
+<td class="t--hd">Status</td>
+<td class="t--hd">Book seats</td>
+</tr>`
+
 const trpsx = `<tr id="tp--itm">
-<td class="t--dt">UG 203FT</td>
+<td class="t--dt"><a href="./trip.html">UG 203FT</a></td>
 <td class="t--dt">Kampala</td>
 <td class="t--dt">Kigali</td>
 <td class="t--dt">3 Sept, 2019</td>
@@ -28,17 +41,11 @@ const trpsx = `<tr id="tp--itm">
      <button class="bk--st">BOOK SEATS</button>
  </a></td>
 </tr>`
+
 notLtrips.innerHTML = `
 <table class="tp--lst">
                <tr class="tp--thd">
-                   <td class="t--hd">Bus</td>
-                   <td class="t--hd">Origin</td>
-                   <td class="t--hd">Destination</td>
-                   <td class="t--hd">Date</td>
-                   <td class="t--hd">Fare</td>
-                   <td class="t--hd">Status</td>
-                   <td class="t--hd">Book seats</td>
-               </tr>
+            ${tpst}
                <!-- Not logged in trips -->
             ${trpsx}
             ${trpsx}
