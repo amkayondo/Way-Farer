97cd86699dc38bc6dd0797a1e62f43cfb9aed489
amkayondo <amkayondo01@gmail.com>
2019-07-15 01:54:55 +0300

    add password harshing and token generation to app

--- server/tests/api.test.js
+++ server/tests/api.test.js
@@ -2,9 +2,22 @@ import chai, { expect } from 'chai';
 import { describe, it } from 'mocha';
 import chaiHttp from 'chai-http';
 import app from '../index';
+// import createToken from '../helpers/users/token';
+
+const uuid = require('uuid');
 
 
 chai.use(chaiHttp);
+const newUser = {
+  id: uuid.v4(),
+  firstName: 'kayondo',
+  lastName: 'edward',
+  email: 'kayondo@open.co',
+  password: '23456',
+  admin: false,
+};
+
+// const newToken = createToken(newUser);
 
 describe('API', () => {
   it('should return a string', (done) => {
@@ -18,14 +31,10 @@ describe('API', () => {
   it('should create an account', (done) => {
     chai.request(app)
       .post('/api/v1/auth/signup')
-      .send({
-        firstName: 'tom',
-        lastName: 'timo',
-        email: 'timo@gio.com',
-        password: '12345',
-      })
+      .send(newUser)
       .end((err, res) => {
         expect(res).to.have.status(200);
+        expect(res.body).to.be.an('array');
       });
     done();
   });
