2756c3aed1608f899429408bc7618fd7240b4555
amkayondo <amkayondo01@gmail.com>
2019-07-17 13:18:56 +0300

    refactor user code structures for maintainability

--- server/helpers/users/token.js
+++ server/helpers/users/token.js
@@ -1,8 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const createToken = (payload) => {
-  return jwt.sign(payload,
-    'THIS IS MY SECRETE', { expiresIn: '7d' });
-};
+const createToken = payload => jwt.sign(payload,
+  'THIS IS MY SECRETE', { expiresIn: '7d' });
 
 export default createToken;
