d389fb1069cc2d96e20a020546687815f8ee334a
amkayondo <amkayondo01@gmail.com>
2019-08-08 14:25:30 +0200

    bug(api): Fix date bug
    -  remove un neccessary code that causes date bug
    
    [Finishes #5438233]

--- server/controllers/trips/createTrip.js
+++ server/controllers/trips/createTrip.js
@@ -22,12 +22,11 @@ const createTrip = (req, res) => {
     if (error) {
       return resPonse.errorMessage(res, 400, (error.details[0].message));
     }
-    const busBooked = Trip.tripDataBase.find(x => x.tripDate === tripDate);
     const isBus = Trip.tripDataBase.find(x => x.busLicenseNumber === busLicenseNumber);
     if (isBus) {
       return resPonse.errorMessage(
         res, 400,
-        `A bus with License Number ${busLicenseNumber} is already booked on ${busBooked.tripDate}`,
+        `A bus with License Number ${busLicenseNumber} is already booked`,
       );
     }
     Trip.creatAtrip(data);
