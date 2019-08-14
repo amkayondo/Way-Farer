d2a0f1efb03d75df46a16177b2d70de16a7fcfe1
amkayondo <amkayondo01@gmail.com>
2019-08-14 22:13:28 +0200

    chore(API) Fix bugs - fix th null value on availableseats - admin cant cancel a trip twice - users cant see cancelled trips

--- server/controllers/trips/getTrips.js
+++ server/controllers/trips/getTrips.js
@@ -8,16 +8,21 @@ const getAllTrips = async (req, res) => {
   try {
     const foundtrips = await trip.getAllTrips();
     const tripData = foundtrips.rows;
+    if (foundtrips.rowCount === 0){
+      return resPonse.errorMessage(res, 404, 'No trips available at the moment');
+    }
     const activeTrips = tripData.filter(x => x.status === 'active');
     const admin = jwt.decode(req.headers.authorization);
     if (admin.isadmin === true){
       return resPonse.successDatas(res, 200, foundtrips.rows.length, foundtrips.rows);
     }
     if (!admin.isadmin === true) {
-      return resPonse.successDatas(res, 200, foundtrips.rows.length, activeTrips);
-    } resPonse.errorMessage(res, 404, 'No trips available at the moment');
+      if (activeTrips.length > 0){
+        return resPonse.successDatas(res, 200, activeTrips.length, activeTrips);
+      } resPonse.errorMessage(res, 404, 'No trips available at the moment');
+    }
   } catch (err) {
-    resPonse.errorMessage(res, 500, err.message);
+    console.log(err.message);
   }
 };
 module.exports = getAllTrips;
