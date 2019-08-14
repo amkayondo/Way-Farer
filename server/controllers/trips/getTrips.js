56c12f8e819f163b31b08c0715e50a919410e777
amkayondo <amkayondo01@gmail.com>
2019-08-14 13:43:57 +0200

    chore(API) Modify validations and implement feedback - validate data types - validate trip id - user shouldnt see cancelled trips

--- server/controllers/trips/getTrips.js
+++ server/controllers/trips/getTrips.js
@@ -1,3 +1,4 @@
+import jwt from 'jsonwebtoken';
 import resPonse from '../../helpers/responses/response';
 import Trip from '../../models/trips';
 
@@ -6,8 +7,14 @@ const trip = new Trip();
 const getAllTrips = async (req, res) => {
   try {
     const foundtrips = await trip.getAllTrips();
-    if (foundtrips.rows.length > 0) {
+    const tripData = foundtrips.rows;
+    const activeTrips = tripData.filter(x => x.status === 'active');
+    const admin = jwt.decode(req.headers.authorization);
+    if (admin.isadmin === true){
       return resPonse.successDatas(res, 200, foundtrips.rows.length, foundtrips.rows);
+    }
+    if (!admin.isadmin === true) {
+      return resPonse.successDatas(res, 200, foundtrips.rows.length, activeTrips);
     } resPonse.errorMessage(res, 404, 'No trips available at the moment');
   } catch (err) {
     resPonse.errorMessage(res, 500, err.message);
