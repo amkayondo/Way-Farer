069df61438c0f283736a8fcdc4f8c07ed38d7622
amkayondo <amkayondo01@gmail.com>
2019-08-16 03:51:28 +0200

    fix bugs and finixh up feedback

--- server/tests/main.test.js
+++ server/tests/main.test.js
@@ -2,6 +2,7 @@ import runUserTests from './user.test';
 import runTripTests from './trips.test';
 import runBookingTests from './bookings.test';
 
+
 runUserTests();
 runTripTests();
 runBookingTests();
