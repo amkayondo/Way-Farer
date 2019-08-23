176b5c1d0ad3b891cdb83f92c74417466a9e9e0a
amkayondo <amkayondo01@gmail.com>
2019-08-23 22:03:53 +0200

    ch(UI): refactor code - rmove duplications

--- UI/js/adminMenu.js
+++ UI/js/adminMenu.js
@@ -1,3 +1,4 @@
+import Main from './main';
 const usr_img = document.getElementById('usdds');
 const usr_menu = document.getElementById('uspdd');
 const mdl = document.getElementById('modl');
@@ -5,11 +6,5 @@ const mdl = document.getElementById('modl');
 usr_menu.style.display = 'none';
 mdl.style.display = 'none';
 
-const nakeChange = (main, div1, div2, action1, action2) => {
-    main.addEventListener('click', () => {
-    div1.style.display = action1;
-    div2.style.display = action2;
-})}
-
-nakeChange(usr_img, usr_menu, mdl, 'block', 'block');
-nakeChange(mdl, usr_menu, mdl, 'none', 'none');
+Main.MakeChange(usr_img, usr_menu, mdl, 'block', 'block');
+Main.MakeChange(mdl, usr_menu, mdl, 'none', 'none');
